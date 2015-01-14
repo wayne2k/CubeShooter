862bdae44082c480415a72c7477df04348c7860c
wayne2k <wayne2k@gmail.com>
2015-01-14 22:51:54 +0200

    Sine Wave Testing
    Testing sine wave movement
    
    Works but cube does not return or stay in center while one sine wave.
    consider using sine direction as  -1 or 1 instead on actual value.
    
    minor scene change for testing of cube movement.
    
    add AI script to controll movement.
    sinedirection has to be applied in fixedupdate
    
    See unity space shooter for evade script.
    
    side note.
    trails work consider using it for visual effects

---
+++ Assets/CubeShooter_Space/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections;
+
+namespace RollRoti.CubeShooter_Space
+{
+	[RequireComponent (typeof (Rigidbody))]
+	public class EnemyMovement : MonoBehaviour 
+	{
+		public bool canMove = true;
+		public float speed = 2f;
+		public Vector3 moveDirection;
+		public SineWave sineWave;
+
+
+		bool _stoppedMoving = false;
+		[SerializeField] Vector3 _currentVelocity;
+
+		void Awake ()
+		{
+			rigidbody.useGravity = false;
+			rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+
+			moveDirection = transform.forward;
+			sineWave.sineAxis = transform.right;
+		}
+		
+		void FixedUpdate ()
+		{
+			if (Input.GetAxisRaw ("Vertical") > 0f)
+			    Move ();
+			else if (Input.GetAxisRaw ("Vertical") < 0f)
+				Stop ();
+
+			if (canMove)
+				rigidbody.velocity += sineWave.Direction;
+
+			_currentVelocity = rigidbody.velocity;
+		}
+
+		public void Move ()
+		{
+			if (canMove) 
+			{
+				rigidbody.velocity = (moveDirection + sineWave.Direction) * speed;	
+			}
+		}
+
+		public void Stop ()
+		{
+			rigidbody.velocity = Vector3.zero;
+		}
+	}
+
+	[System.Serializable]
+	public class SineWave
+	{
+		public bool useSineWave = false;
+		public float frequency = 10.0f; // Speed of sine movement
+		public float magnitude = 0.5f; // Size of sine movement
+		public Vector3 sineAxis = Vector3.right;
+
+		public Vector3 Direction { 
+			get { 
+				if (useSineWave == false)
+					return Vector3.zero;
+
+				return sineAxis * Mathf.Sin (Time.time * frequency) * magnitude; 
+			} 
+		}
+	}
+}
