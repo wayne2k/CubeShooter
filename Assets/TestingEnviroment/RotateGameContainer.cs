66b6e9564df648ac1dcabf66b48128005c082157
wayne2k <wayne2k@gmail.com>
2015-01-24 18:22:45 +0200

    Playing around with the enviroment
    Just trying out different enviroment stuff.

---
+++ Assets/TestingEnviroment/RotateGameContainer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateGameContainer : MonoBehaviour
+{
+	public float speed = 2f;
+
+	public Vector3 axis = Vector3.up;
+
+	void Update ()
+	{
+		transform.Rotate (axis * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/TestingEnviroment/TextureScroller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace RollRoti.CubeShooter_Space
+{
+	public class TextureScroller : MonoBehaviour
+	{
+		public Vector2 scrollAxis = Vector3.up;
+		public float speed = 0.1f;
+
+		Vector2 offset = Vector2.zero;
+		Vector2 _initOffset;
+
+		void Awake ()
+		{
+			_initOffset = renderer.sharedMaterial.GetTextureOffset ("_MainTex");
+		}
+
+		void Update ()
+		{
+			float y = Mathf.Repeat (Time.time * speed, 1);
+
+			if (scrollAxis.x != 0)
+				offset.x = y;
+
+			if (scrollAxis.y != 0)
+				offset.y = y;
+
+			renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+		}
+
+		void OnDisable () 
+		{
+			renderer.sharedMaterial.SetTextureOffset ("_MainTex", _initOffset);
+		}
+	}
+}
