a1f565e5744216ef2c85565a14cfe36d3086f75a
wayne2k <wayne2k@gmail.com>
2015-01-28 19:10:02 +0200

    New Spawner Script
    New Spawner script added.
    spawns between two transforms.
    
    Some code added to ExtenstionsMethod Script.

--- Assets/CubeShooter_Space/Scripts/Helpers/ExtensionMethods.cs
+++ Assets/CubeShooter_Space/Scripts/Helpers/ExtensionMethods.cs
@@ -5,9 +5,25 @@ namespace RollRoti.HelperLib
 {
 	public static class ExtensionMethods
 	{
+		public static Vector3 RandomVector3 (Vector3 min, Vector3 max)
+		{
+			Vector3 _vector3 = Vector3.zero;
+
+			_vector3.x = Random.Range (min.x, max.x);
+			_vector3.y = Random.Range (min.y, max.y);
+			_vector3.z = Random.Range (min.z, max.z);
+
+			return _vector3;
+		}
+
 		public static float RandomFromRange (this Vector2 value) 
 		{
 			return Random.Range (value.x, value.y);
+		}
+
+		public static int RandomIntFromRange (this Vector2 value) 
+		{
+			return Random.Range ((int) value.x, (int) value.y);
 		}
 
 		public static bool ToBool (this int value)
